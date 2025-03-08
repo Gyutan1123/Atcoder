25790aa07d209064bc78883bd26ab10e613b9373
Gyutan <raito1123@ezweb.ne.jp>
2025-03-08 23:27:09 +0900

    テンプレートファイル(暫定)

---
+++ template.py
@@ -0,0 +1,11 @@
+import sys
+
+
+II = lambda: int(sys.stdin.readline().rstrip())
+SI = lambda: sys.stdin.readline().rstrip()
+MI = lambda: map(int, sys.stdin.readline().rstrip().split())
+MS = lambda: sys.stdin.readline().rstrip().split()
+LI = lambda: list(map(int, sys.stdin.readline().rstrip().split()))
+LS = lambda: list(sys.stdin.readline().rstrip().split())
+
+########################################################
