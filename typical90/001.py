416d0f8d7d8b2561e8e99b03e631f3928d4adc25
Gyutan <raito1123@ezweb.ne.jp>
2025-03-11 01:29:37 +0900

    001 リファクタリング

--- typical90/001.py
+++ typical90/001.py
@@ -13,8 +13,6 @@ LS = lambda: list(sys.stdin.readline().rstrip().split())
 n,l = MI()
 k = II()
 A = LI()
-A.insert(0,0)
-
 left = 0
 right = l
 
@@ -22,21 +20,20 @@ while right - left > 1:
   mid = (right + left) // 2
   
   # 長さがmid以上で最小になるように切る
-  tmpSum = 0
-  cutCount = 0
-  rightEndCutIndex = 0
+  pieceCount = 0 
+  preCutPos = 0
   for i in range(n):
-    tmpSum += A[i+1] - A[i] 
-    if tmpSum >= mid:
-      tmpSum = 0
-      cutCount += 1
-      rightEndCutIndex = i+1
+    if A[i] - preCutPos >= mid:
+      pieceCount += 1
+      preCutPos = A[i]
+      
+  if l - preCutPos >= mid:
+    pieceCount += 1
     
   # スコア mid 以上は可能 
-  if cutCount > k or (cutCount == k and l - A[rightEndCutIndex] >= mid):
+  if pieceCount >= k+1:
     left = mid
   else:
     right = mid
     
 print(left)
-       
