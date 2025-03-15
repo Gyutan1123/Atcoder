b8662c70a416f972bfab723b51e9825100d69cdb
Gyutan <raito1123@ezweb.ne.jp>
2025-03-16 00:17:30 +0900

    008 - AtCounter

--- typical90/008.py
+++ typical90/008.py
@@ -1,6 +1,5 @@
 import sys
 import collections, heapq, string
-
 II = lambda: int(sys.stdin.readline().rstrip())
 SI = lambda: sys.stdin.readline().rstrip()
 MI = lambda: map(int, sys.stdin.readline().rstrip().split())
@@ -10,3 +9,24 @@ LS = lambda: list(sys.stdin.readline().rstrip().split())
 
 mod = 10**9 + 7
 ########################################################
+
+n = II()
+s = SI()
+
+mod = 10**9 + 7
+
+dp = [[0]*(n+1) for _ in range(8)]
+
+str = '?atcoder'
+
+for j in range(n+1):
+  dp[0][j] = 1
+
+for i in range(1,8):
+  for j in range(n):
+    if s[j] == str[i]:
+      dp[i][j+1] = (dp[i][j] + dp[i-1][j]) % mod
+    else:
+      dp[i][j+1] = dp[i][j] % mod
+
+print(dp[7][n] % mod)
