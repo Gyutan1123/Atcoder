6b9208d74e8146218637fba103ea0b10cd5b06e5
Gyutan <raito1123@ezweb.ne.jp>
2025-03-16 20:27:41 +0900

    029 - Long Bricks

---
+++ lib/SegTree.py
@@ -0,0 +1,310 @@
+# セグ木
+# https://qiita.com/AkariLuminous/items/32cbf5bc3ffb2f84a898
+class SegTree:
+  def __init__(self, op, e, n, v=None):
+    """
+    Args:
+        op : 演算
+        e : 単位元
+        n : 対象の列の要素数
+        v : 対象の列. Defaults to None.
+    """
+    self._n = n
+    self._op = op
+    self._e = e
+    self._log = (n-1).bit_length() # 2^(_log) >= n となる最小の整数
+    self._size = 1 << self._log
+    self._d = [self._e()] * (2 * self._size)
+    if v is not None:
+      # 葉に対象の列を格納
+      for i in range(self._n):
+        self._d[self._size + i] = v[i]
+      # 葉に近い場所から順に更新
+      for i in range(self._size-1, 0, -1):
+        self._update(i)
+        
+  def _update(self, k):
+    """親をノードを更新する
+    """
+    self._d[k] = self._op(self._d[2*k], self._d[2*k+1])
+    
+  def set(self, p, x):
+    """ 更新クエリ
+
+    Args:
+        p : a_p を 
+        x : xに更新
+    """
+    # 葉に移動
+    p += self._size
+    
+    self._d[p] = x
+    # 関連する場所を更新
+    for i in range(1, self._log+1):
+      self._update(p >> i)
+      
+  def prod(self, l, r):
+    """ 取得クエリ
+
+    Args:
+        l, r : 区間[l, r)
+    Return:
+        区間[l, r)の総積
+    """
+    # 左の結果、右の結果
+    sml, smr = self._e(), self._e()
+    
+    l += self._size
+    r += self._size
+    
+    # 未計算の区間がなくなるまで
+    while l < r:
+      # 自身が右子ノードなら使用
+      if l & 1:
+        sml = self._op(sml, self._d[l])
+        l += 1
+      if r & 1:
+        r -= 1
+        smr = self._op(self._d[r], smr)
+        
+      # 親に移動
+      l >>= 1
+      r >>= 1
+    return self._op(sml, smr)
+  
+  def all_prod(self):
+    """ 全要素の総積を取得
+    """
+    return self._d[1]
+  
+  def get(self, p):
+    """ a_pを取得するメソッド
+    """
+    return self._d[p + self._size]
+  
+# 遅延セグ木(区間加算用）
+# https://qiita.com/takayg1/items/b7b3f7d458915bcc7a4e
+class LazySegTree_RAQ:
+    """
+    init(init_val, ide_ele): 配列init_valで初期化 O(N)
+    add(l, r, x): 区間[l, r)にxを加算 O(logN)
+    query(l, r): 区間[l, r)をsegfuncしたものを返す O(logN)
+    """
+    def __init__(self, init_val, segfunc, ide_ele):
+        """
+        init_val: 配列の初期値
+        segfunc: 区間にしたい操作
+        ide_ele: 単位元
+        num: n以上の最小の2のべき乗
+        data: 値配列(1-index)
+        lazy: 遅延配列(1-index)
+        """
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.data = [ide_ele] * 2 * self.num
+        self.lazy = [0] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.data[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1])
+
+    def gindex(self, l, r):
+            """
+            伝搬する対象の区間を求める
+            lm: 伝搬する必要のある最大の左閉区間
+            rm: 伝搬する必要のある最大の右開区間
+            """
+            l += self.num
+            r += self.num
+            lm = l >> (l & -l).bit_length()
+            rm = r >> (r & -r).bit_length()
+
+            while r > l:
+                if l <= lm:
+                    yield l
+                if r <= rm:
+                    yield r
+                r >>= 1
+                l >>= 1
+            while l:
+                yield l
+                l >>= 1
+
+    def propagates(self, *ids):
+        """
+        遅延伝搬処理
+        ids: 伝搬する対象の区間 
+        """
+        for i in reversed(ids):
+            v = self.lazy[i]
+            if not v:
+                continue
+            self.lazy[2 * i] += v
+            self.lazy[2 * i + 1] += v
+            self.data[2 * i] += v
+            self.data[2 * i + 1] += v
+            self.lazy[i] = 0
+
+    def add(self, l, r, x):
+        """
+        区間[l, r)の値にxを加算
+        l, r: index(0-index)
+        x: additional value
+        """
+        *ids, = self.gindex(l, r)
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                self.lazy[l] += x
+                self.data[l] += x
+                l += 1
+            if r & 1:
+                self.lazy[r - 1] += x
+                self.data[r - 1] += x
+            r >>= 1
+            l >>= 1
+        for i in ids:
+            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1]) + self.lazy[i]
+
+
+    def query(self, l, r):
+        """
+        [l, r)のsegfuncしたものを得る
+        l: index(0-index)
+        r: index(0-index)
+        """
+        *ids, = self.gindex(l, r)
+        self.propagates(*ids)
+
+        res = self.ide_ele
+
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.data[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.data[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+      
+# 遅延セグ木(区間更新用)
+# https://qiita.com/takayg1/items/b7b3f7d458915bcc7a4e
+class LazySegTree_RUQ:
+    """
+    init(init_val, ide_ele): 配列init_valで初期化 O(N)
+    update(l, r, x): 区間[l, r)をxに更新 O(logN)
+    query(l, r): 区間[l, r)をsegfuncしたものを返す O(logN)
+    """
+    def __init__(self, init_val, segfunc, ide_ele):
+        """
+        init_val: 配列の初期値
+        segfunc: 区間にしたい操作
+        ide_ele: 単位元
+        num: n以上の最小の2のべき乗
+        data: 値配列(1-index)
+        lazy: 遅延配列(1-index)
+        """
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.data = [ide_ele] * 2 * self.num
+        self.lazy = [None] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.data[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1])
+
+    def gindex(self, l, r):
+            """
+            伝搬する対象の区間を求める
+            lm: 伝搬する必要のある最大の左閉区間
+            rm: 伝搬する必要のある最大の右開区間
+            """
+            l += self.num
+            r += self.num
+            lm = l >> (l & -l).bit_length()
+            rm = r >> (r & -r).bit_length()
+         
+            while r > l:
+                if l <= lm:
+                    yield l
+                if r <= rm:
+                    yield r
+                r >>= 1
+                l >>= 1
+            while l:
+                yield l
+                l >>= 1
+
+    def propagates(self, *ids):
+        """
+        遅延伝搬処理
+        ids: 伝搬する対象の区間 
+        """
+        for i in reversed(ids):
+            v = self.lazy[i]
+            if v is None:
+                continue
+            self.lazy[2 * i] = v
+            self.lazy[2 * i + 1] = v
+            self.data[2 * i] = v
+            self.data[2 * i + 1] = v
+            self.lazy[i] = None
+
+    def update(self, l, r, x):
+        """
+        区間[l, r)の値をxに更新
+        l, r: index(0-index)
+        x: update value
+        """
+        *ids, = self.gindex(l, r)
+        self.propagates(*ids)
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                self.lazy[l] = x
+                self.data[l] = x
+                l += 1
+            if r & 1:
+                self.lazy[r - 1] = x
+                self.data[r - 1] = x
+            r >>= 1
+            l >>= 1
+        for i in ids:
+            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1])
+
+
+    def query(self, l, r):
+        """
+        [l, r)のsegfuncしたものを得る
+        l: index(0-index)
+        r: index(0-index)
+        """
+        *ids, = self.gindex(l, r)
+        self.propagates(*ids)
+
+        res = self.ide_ele
+
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.data[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.data[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
