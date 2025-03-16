741a5d5913562405db596752f5bc0591b45f9507
Gyutan <raito1123@ezweb.ne.jp>
2025-03-16 16:19:49 +0900

    020 - Log Inequality

---
+++ typical90/020.py
@@ -0,0 +1,15 @@
+import sys
+import collections, heapq, string
+II = lambda: int(sys.stdin.readline().rstrip())
+SI = lambda: sys.stdin.readline().rstrip()
+MI = lambda: map(int, sys.stdin.readline().rstrip().split())
+MS = lambda: sys.stdin.readline().rstrip().split()
+LI = lambda: list(map(int, sys.stdin.readline().rstrip().split()))
+LS = lambda: list(sys.stdin.readline().rstrip().split())
+
+mod = 10**9 + 7
+########################################################
+
+a,b,c = MI()
+
+print('Yes' if a < pow(c,b) else 'No')
