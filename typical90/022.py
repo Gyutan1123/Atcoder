8876fe3fe8e7dc997d17f72555a2cd66cf7c010b
Gyutan <raito1123@ezweb.ne.jp>
2025-03-16 18:05:43 +0900

    022 - Cubic Cake

--- typical90/022.py
+++ typical90/022.py
@@ -1,6 +1,5 @@
 import sys
 import collections, heapq, string, math
-
 II = lambda: int(sys.stdin.readline().rstrip())
 SI = lambda: sys.stdin.readline().rstrip()
 MI = lambda: map(int, sys.stdin.readline().rstrip().split())
@@ -10,3 +9,8 @@ LS = lambda: list(sys.stdin.readline().rstrip().split())
 
 mod = 10**9 + 7
 ########################################################
+
+a,b,c = MI()
+
+gcd = math.gcd(a,b,c)
+print((a//gcd-1)+(b//gcd-1)+(c//gcd-1))
