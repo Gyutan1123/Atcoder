47d727baa62c754bd9905d7e40fb9365b4e40ef8
Gyutan <raito1123@ezweb.ne.jp>
2025-03-19 21:32:42 +0900

    038 - Large LCM

---
+++ typical90/038.py
@@ -0,0 +1,15 @@
+import sys
+import collections, heapq, string, math, itertools
+II = lambda: int(sys.stdin.readline().rstrip())
+SI = lambda: sys.stdin.readline().rstrip()
+MI = lambda: map(int, sys.stdin.readline().rstrip().split())
+MS = lambda: sys.stdin.readline().rstrip().split()
+LI = lambda: list(map(int, sys.stdin.readline().rstrip().split()))
+LS = lambda: list(sys.stdin.readline().rstrip().split())
+
+mod = 10**9 + 7
+########################################################
+
+a,b = MI()
+lcm = math.lcm(a,b)
+print(lcm if lcm <= 10**18 else 'Large')
