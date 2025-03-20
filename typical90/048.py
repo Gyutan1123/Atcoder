ccfa75d3c8773f3b6a53ee356f3e0e9981d85636
Gyutan <raito1123@ezweb.ne.jp>
2025-03-20 14:32:40 +0900

    048 - I will not drop out

---
+++ typical90/048.py
@@ -0,0 +1,23 @@
+import sys
+import collections, heapq, string, math, itertools
+
+II = lambda: int(sys.stdin.readline().rstrip())
+SI = lambda: sys.stdin.readline().rstrip()
+MI = lambda: map(int, sys.stdin.readline().rstrip().split())
+MS = lambda: sys.stdin.readline().rstrip().split()
+LI = lambda: list(map(int, sys.stdin.readline().rstrip().split()))
+LS = lambda: list(sys.stdin.readline().rstrip().split())
+
+sys.setrecursionlimit(10**7)
+mod = 10**9 + 7
+########################################################
+
+n,k = MI()
+score = []
+for _ in range(n):
+  a,b = MI()
+  score.append(a-b)
+  score.append(b)
+
+score.sort(reverse=True)
+print(sum(score[:k]))
