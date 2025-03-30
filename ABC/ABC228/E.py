2feb1424818c05fd9c5c8e83c25e92280184f093
Gyutan <raito1123@ezweb.ne.jp>
2025-03-30 15:06:13 +0900

    ABC228

---
+++ ABC/ABC228/E.py
@@ -0,0 +1,23 @@
+import sys
+import collections, heapq, string, math, itertools, copy
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+# pypyで再帰書く時のおまじない
+# pypyjit.set_param('max_unroll_recursion=-1')
+# import pypyjit
+
+II = lambda: int(input())
+SI = lambda: input()
+MI = lambda: map(int, input().split())
+MS = lambda: input().split()
+LI = lambda: list(MI())
+LS = lambda: list(MS())
+
+sys.setrecursionlimit(10**7)
+mod = 998244353
+########################################################
+n,k,m = MI()
+if m % mod == 0:
+  print(0)
+else:
+  print(pow(m,pow(k,n,mod=mod-1),mod))
