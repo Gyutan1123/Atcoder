c8702b96eaecda55fe0d83118f8ddc738d12df3e
Gyutan <raito1123@ezweb.ne.jp>
2025-04-22 23:16:18 +0900

    ADTの分

---
+++ ABC/ABC201~250/ABC229/C.py
@@ -0,0 +1,35 @@
+import sys
+import collections, heapq, string, math, itertools, copy, bisect
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+# pypyで再帰書く時のおまじない
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
+
+II = lambda: int(input())
+SI = lambda: input()
+MI = lambda: map(int, input().split())
+MS = lambda: input().split()
+LI = lambda: list(MI())
+LS = lambda: list(MS())
+
+sys.setrecursionlimit(10**7)
+mod = 10**9 + 7
+########################################################
+
+n,w = MI()
+
+c = [LI() for _ in range(n)]
+
+c.sort(reverse=True)
+
+tmp = 0
+ans = 0
+for i in range(n):
+  a,b = c[i]
+  use = min(b, w-tmp)
+  
+  tmp += use
+  ans += a*use
+  
+print(ans)
