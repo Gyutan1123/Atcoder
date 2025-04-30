0bfae5bae55bc116f908e75f5d64bf4deda96872
Gyutan <raito1123@ezweb.ne.jp>
2025-04-30 17:22:02 +0900

    createfiles.py をADTに対応させた

--- createfiles.py
+++ createfiles.py
@@ -1,21 +1,27 @@
 import os
 import shutil
 import argparse
+import datetime
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("prefix")
-    parser.add_argument("number")
+    parser.add_argument("-n", "--number",required=False, default="")
     args = parser.parse_args()
+          
 
     base_dir = args.prefix
     target_subdir = f"{args.prefix}{args.number}"
     target_dir = os.path.join(base_dir, target_subdir)
-    
-    os.makedirs(target_dir, exist_ok=True)
-  
+
     template_file = "template.py"
     files_to_create = ["A.py", "B.py", "C.py", "D.py", "E.py","F.py","G.py"]
+
+    if args.prefix == 'ADT':
+      target_dir = datetime.datetime.today().strftime("%Y-%m-%d")
+      files_to_create = ["A.py", "B.py", "C.py", "D.py", "E.py","F.py","G.py","H.py","I.py"]
+      
+    os.makedirs(target_dir, exist_ok=True)
     
     for filename in files_to_create:
         dest_file = os.path.join(target_dir, filename)
