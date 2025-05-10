2d19e24a4f2c9ba833ab29fab6cd6ca6b5e88040
Gyutan <raito1123@ezweb.ne.jp>
2025-05-10 23:10:38 +0900

    ABC405 after

---
+++ ABC/ABC401~450/ABC405/F.py
@@ -0,0 +1,121 @@
+import sys
+import collections, heapq, string, math, itertools, copy, bisect, functools
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+# pypyで再帰書く時のおまじない
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
+
+II = lambda: int(input())
+SI = lambda: input()
+MI = lambda: map(int, input().split())
+MS = lambda: input().split()
+LI = lambda: list(MI())
+LS = lambda: list(MS())
+
+sys.setrecursionlimit(10**7)
+mod = 10**9 + 7
+########################################################
+
+class SegTree:
+  def __init__(self, op, e, n, v=None):
+    """
+    Args:
+        op : 演算
+        e : 単位元
+        n : 対象の列の要素数
+        v : 対象の列. Defaults to None.
+    """
+    self._n = n
+    self._op = op
+    self._e = e
+    self._log = (n-1).bit_length() # 2^(_log) >= n となる最小の整数
+    self._size = 1 << self._log
+    self._d = [self._e()] * (2 * self._size)
+    if v is not None:
+      # 葉に対象の列を格納
+      for i in range(self._n):
+        self._d[self._size + i] = v[i]
+      for i in range(self._size - 1, 0, -1):
+        self._d[i] = self._op(self._d[i << 1], self._d[i << 1 | 1])
+            
+  def set(self, p, x):
+    """ 更新クエリ
+
+    Args:
+        p : a_p を 
+        x : xに更新
+    """
+    # 葉に移動
+    p += self._size
+    
+    self._d[p] = x
+    # 関連する場所を更新
+    while p:
+      self._d[p >> 1] = self._op(self._d[p], self._d[p ^ 1])
+      p >>= 1
+      
+  def prod(self, l, r):
+    """ 取得クエリ
+
+    Args:
+        l, r : 区間[l, r)
+    Return:
+        区間[l, r)の総積
+    """
+    # 左の結果、右の結果
+    sml, smr = self._e(), self._e()
+    
+    l += self._size
+    r += self._size
+    
+    # 未計算の区間がなくなるまで
+    while l < r:
+      # 自身が右子ノードなら使用
+      if l & 1:
+        sml = self._op(sml, self._d[l])
+        l += 1
+      if r & 1:
+        r -= 1
+        smr = self._op(self._d[r], smr)
+        
+      # 親に移動
+      l >>= 1
+      r >>= 1
+    return self._op(sml, smr)
+  
+  def all_prod(self):
+    """ 全要素の総積を取得
+    """
+    return self._d[1]
+  
+  def get(self, p):
+    """ a_pを取得するメソッド
+    """
+    return self._d[p + self._size]
+n,m = MI()
+
+AB = [LI()+[i] for i in range(m)]
+q = II()
+CD = [LI()+[i] for i in range(q)]
+
+chord = AB+CD
+chord.sort(key=lambda x: (x[0], -x[1]))
+
+s = SegTree(lambda x,y: x+y, lambda: 0, 2*n+1)
+ans = [0]*q
+for l,r,i in chord:
+  if l%2 == 1:
+    ans[i] = s.prod(l,r)
+  else:
+    s.set(r, 1)
+s = SegTree(lambda x,y: x+y, lambda: 0, 2*n+1)
+chord.sort(key=lambda x: (x[1],-x[0]))
+for l,r,i in reversed(chord):
+  if l%2 == 1:
+    ans[i] += s.prod(l,r)
+  else:
+    s.set(l, 1)
+
+for a in ans:
+  print(a)
